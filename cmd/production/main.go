d11743c4779bddedcb0abee9828fcca169798b7e
Tantsevov Grigory <tancevov.g@wildberries.ru>
2019-12-04 18:36:19 +0300

    back

--- cmd/production/main.go
+++ cmd/production/main.go
@@ -16,7 +16,7 @@ var (
 )
 
 func main() {
-	rules, err := loadRules(*fileName)
+	rules, backRules, err := loadRules(*fileName)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -25,18 +25,30 @@ func main() {
 	for k, v := range rules {
 		fmt.Printf("%s -> %v\n", k, v)
 	}
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Println()
-		fmt.Println("Введите факты:")
-		text, _, _ := reader.ReadLine()
-		fmt.Println()
-		wq := proccessForward(text, rules)
-		fmt.Println()
-		fmt.Println("Итоговая рабочая память:")
-		fmt.Println(wq[len(wq)-1])
 
+	fmt.Println()
+	fmt.Println("БЗ в обратном порядке")
+	for k, v := range backRules {
+		fmt.Println(k, "<-", v)
 	}
+	fmt.Println()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println()
+	fmt.Println("Введите факты:")
+	text, _, _ := reader.ReadLine()
+	fmt.Println()
+	wq := proccessForward(text, rules)
+	fmt.Println()
+	fmt.Println("Итоговая рабочая память:")
+	fmt.Println(wq[len(wq)-1])
+
+	fmt.Println()
+	fmt.Println("Введите факт для проверки:")
+	text, _, _ = reader.ReadLine()
+	fmt.Println()
+	proccessBack(string(text), backRules)
 }
 
 // Прямой ход для набора фактов
@@ -73,6 +85,37 @@ func proccessForward(baseRulesJSON []byte, rules map[string][]string) []string {
 	return workQueue
 }
 
+func proccessBack(goal string, backRules map[string][]string) {
+	workRules := make(map[string][]string, len(backRules))
+	for k, v := range backRules {
+		workRules[k] = append([]string{}, v...)
+	}
+
+	stack := []string{goal}
+	for len(stack) != 0 || len(workRules) != 0 {
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		rightFacts, ok := workRules[top]
+		if !ok {
+			fmt.Println("В БЗ нет правила для вывода факта", top)
+			return
+		}
+		delete(workRules, top)
+		for _, rightFact := range rightFacts {
+			var tmp []string
+			json.Unmarshal([]byte(rightFact), &tmp)
+			for _, newGoal := range tmp {
+				if _, ok := workRules[newGoal]; !ok {
+					fmt.Println("В БЗ нет правила для вывода факта", newGoal, "который является частью факта", tmp)
+					return
+				}
+				stack = append(stack, newGoal)
+			}
+			fmt.Println(top, "<-", tmp)
+		}
+	}
+}
+
 func addToWorkQueue(workQueue []string, rule string) []string {
 	l := len(workQueue)
 	inputRule, _ := json.Marshal([]string{rule})
@@ -101,20 +144,22 @@ func contains(sub, check string) bool {
 }
 
 // загрузка правил из файла
-func loadRules(filePath string) (map[string][]string, error) {
+func loadRules(filePath string) (map[string][]string, map[string][]string, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
-		return nil, fmt.Errorf("open rules file: %v", err)
+		return nil, nil, fmt.Errorf("open rules file: %v", err)
 	}
 	var rules []fileRule
 	if err := json.NewDecoder(file).Decode(&rules); err != nil {
-		return nil, fmt.Errorf("parsing rules file: %v", err)
+		return nil, nil, fmt.Errorf("parsing rules file: %v", err)
 	}
 	out := make(map[string][]string, len(rules))
+	back := make(map[string][]string, len(rules))
 	for _, rule := range rules {
 		sort.Strings(rule.Fact)
 		data, _ := json.Marshal(rule.Fact)
 		out[string(data)] = append(out[string(data)], rule.Goal)
+		back[rule.Goal] = append(back[rule.Goal], string(data))
 	}
-	return out, nil
+	return out, back, nil
 }
