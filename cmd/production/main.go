154606d59394e4c95c727cd3ab24bac04c7b6fe5
Tantsevov Grigory <tancevov.g@wildberries.ru>
2019-12-04 17:34:14 +0300

    BFS

--- cmd/production/main.go
+++ cmd/production/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -20,39 +21,73 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("rules:")
+	fmt.Println("Правила в Базе знаний:")
 	for k, v := range rules {
-		fmt.Printf("%s: %v\n", k, v)
+		fmt.Printf("%s -> %v\n", k, v)
 	}
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Println("Input first rule:")
+		fmt.Println()
+		fmt.Println("Введите факты:")
 		text, _, _ := reader.ReadLine()
-		fmt.Println(proccessRuleForward(string(text), rules))
+		fmt.Println()
+		wq := proccessForward(text, rules)
+		fmt.Println()
+		fmt.Println("Итоговая рабочая память:")
+		fmt.Println(wq[len(wq)-1])
+
 	}
 }
 
-func proccessRuleForward(rule string, rules map[string][]string) string {
+// Прямой ход для набора фактов
+func proccessForward(baseRulesJSON []byte, rules map[string][]string) []string {
 	workRules := make(map[string][]string, len(rules))
 	for k, v := range rules {
 		workRules[k] = append([]string{}, v...)
 	}
-	workBuf := rule
+	var workQueue []string
 
-mainLoop:
-	for {
-		for fact, goals := range workRules {
-			if contains(fact, workBuf) {
-				fmt.Println(fact, "->", goals)
-				for _, goal := range goals {
-					workBuf += goal
-					delete(workRules, fact)
-					continue mainLoop
-				}
-			}
-		}
-		return workBuf
+	// Кладем исходные факты в рабочую очередь
+	var baseRules []string
+	json.Unmarshal(baseRulesJSON, &baseRules)
+	for _, rule := range baseRules {
+		workQueue = addToWorkQueue(workQueue, rule)
 	}
+
+	fmt.Println()
+	l := len(workQueue)
+	fmt.Println("Выводы:")
+	for i := 0; i < l; i++ {
+		out, ok := workRules[workQueue[i]]
+		if !ok {
+			continue
+		}
+		for _, fact := range out {
+			fmt.Println(workQueue[i], "->", fact)
+			workQueue = addToWorkQueue(workQueue, fact)
+		}
+
+		l = len(workQueue)
+	}
+
+	return workQueue
+}
+
+func addToWorkQueue(workQueue []string, rule string) []string {
+	l := len(workQueue)
+	inputRule, _ := json.Marshal([]string{rule})
+	// Добавляем в очередь факт
+	workQueue = append(workQueue, string(inputRule))
+	// Добавляем в очередь комбинацию факта с уже имеющимеся
+	for i := 0; i < l; i++ {
+		var tmp []string
+		json.Unmarshal([]byte(workQueue[i]), &tmp)
+		tmp = append(tmp, rule)
+		sort.Strings(tmp)
+		tmpNew, _ := json.Marshal(tmp)
+		workQueue = append(workQueue, string(tmpNew))
+	}
+	return workQueue
 }
 
 // находится ли факт в рабочей памяти
@@ -71,13 +106,15 @@ func loadRules(filePath string) (map[string][]string, error) {
 	if err != nil {
 		return nil, fmt.Errorf("open rules file: %v", err)
 	}
-	var rules []Rule
+	var rules []fileRule
 	if err := json.NewDecoder(file).Decode(&rules); err != nil {
 		return nil, fmt.Errorf("parsing rules file: %v", err)
 	}
 	out := make(map[string][]string, len(rules))
 	for _, rule := range rules {
-		out[rule.Fact] = append(out[rule.Fact], rule.Goal)
+		sort.Strings(rule.Fact)
+		data, _ := json.Marshal(rule.Fact)
+		out[string(data)] = append(out[string(data)], rule.Goal)
 	}
 	return out, nil
 }
--- cmd/production/models.go
+++ cmd/production/models.go
@@ -2,6 +2,12 @@ package main
 
 // Rule описывает правила для продукционной модели
 type Rule struct {
-	Fact string `json:"fact"`
-	Goal string `json:"goal"`
+	Fact string
+	Goal string
+}
+
+// Парсинг файла с правилами
+type fileRule struct {
+	Fact []string `json:"fact"`
+	Goal string   `json:"goal"`
 }
