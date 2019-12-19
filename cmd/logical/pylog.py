c436d7c0ec8e295701ad7837a26198017150c64b
Grigory Tantsevov <tantsevov@gmail.com>
2019-12-20 00:19:14 +0300

    `

--- cmd/logical/pylog.py
+++ cmd/logical/pylog.py
@@ -1,4 +1,5 @@
 from src.Solver import Solver
+import pandas as pd
 
 if __name__ == "__main__":
     with open('examples/logical_rules', 'r') as content_file:
@@ -7,4 +8,8 @@ if __name__ == "__main__":
     print(solver.database)
     while True:
         inp = input("запрос> ")
-        print(solver.find_solutions(inp))
+        solution = solver.find_solutions(inp)
+        if isinstance(solution, dict):
+            print(pd.DataFrame.from_dict(solution))
+        else:
+            print(solution)
